9404d1b1a26ead26c8f20250239ad76e1085ca0b
fuu192 <14249224+fuu192@users.noreply.github.com>
2022-07-21 20:40:42 +0900

    add ./src/builtin/mod.rs
    added builtin/mod.rs file

---
+++ src/builtin/mod.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::io;
+use std::path::Path;
+use std::process;
+
+pub fn exit(code: i32) -> ! {
+    process::exit(code)
+}
+
+pub fn cd(string: &str) -> io::Result<()> {
+    let path = Path::new(string);
+
+    env::set_current_dir(&path)?;
+
+    env::set_var("PWD", path);
+
+    Ok(())
+}
