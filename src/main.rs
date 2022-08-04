6c1189ff30c18b7f0a8351f20fe050383a3c7bf9
fuu192 <14249224+fuu192@users.noreply.github.com>
2022-08-04 15:39:53 +0900

    clean ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+mod ansi;
+mod builtin;
+mod manifest;
 mod parser;
 mod prompt;
-mod variable;
-mod ansi;
-mod manifest;
-mod builtin;
 mod shell;
+mod variable;
 
 fn main() {
-    parser::parse();
+    // println!("{:?}", parser::parse_args(tkns));
+
+    // parser::parse(tkns);
+
+    println!("{:?}", parser::Parser::new(parser::lexer::Lexer::new("echo $PWD")).parse_command());
+
 }
