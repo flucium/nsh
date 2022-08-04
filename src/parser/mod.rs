d62435cee2b3d9580b1aa899fdb9e8a1bc7b9f7e
fuu192 <14249224+fuu192@users.noreply.github.com>
2022-08-04 15:40:17 +0900

    update ./src/parser/mod.rs

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,85 +1,201 @@
 pub mod lexer;
 pub mod token;
 use crate::parser::lexer::Lexer;
-use crate::parser::token::Token;
-use std::cmp::Ordering;
+use crate::parser::token::*;
+use std::mem::swap;
 
-pub fn parse() {
-    let mut tokens = vec![
-        Token::String("ls".to_string()),
-        Token::String("-a".to_string()),
-        Token::Pipe,
-        Token::String("cat".to_string()),
-        Token::String("-b".to_string()),
-        Token::Pipe,
-        Token::String("rev".to_string()),
-        Token::Pipe,
-        Token::String("rev".to_string()),
-    ]
-    .into_iter();
-
-    println!("{:?}", tokens);
+pub struct Parser {
+    tokens: Tokens,
+    tree: Node,
 }
 
-fn parse_command() {}
+impl Parser {
+    pub fn new(mut lexer: Lexer) -> Self {
+        let tokens = lexer.tokenize();
+        Self {
+            tokens: tokens,
+            tree: Node::new(NodeKind::Semicolon),
+        }
+    }
 
-fn parse_equal() {}
+    pub fn parse_command(&mut self) -> Result<Node, ()> {
+        let mut token_buffer = Vec::new();
 
-fn parse_reference(tokens: Vec<Token>) -> Option<Node> {
-    None
+        let left_node = match self.parse_reference() {
+            Ok(ok) => ok,
+
+            Err(_) => {
+                if let Some(token) = self.tokens.pop_front() {
+                    token_buffer.push(token.clone());
+                    match token {
+                        Token::String(string) => {
+                            let mut node = Node::new(NodeKind::Arg);
+
+                            node.value = Some(vec![string]);
+
+                            node
+                        }
+                        _ => {
+                            for token in token_buffer {
+                                self.tokens.push_front(token)
+                            }
+                            return Err(());
+                        }
+                    }
+                } else {
+                    return Err(());
+                }
+            }
+        };
+
+        let right_node = match self.parse_reference() {
+            Ok(ok) => ok,
+
+            Err(_) => {
+                let mut args = Vec::new();
+                while let Some(token) = self.tokens.pop_front() {
+                    match token {
+                        Token::String(string) => args.push(string),
+                        _ => {
+                            self.tokens.push_front(token);
+                            break;
+                        }
+                    }
+                }
+
+                let node_kind = if args.len() > 1 {
+                    NodeKind::Args
+                } else {
+                    NodeKind::Arg
+                };
+
+                let mut node = Node::new(node_kind);
+
+                node.value = Some(args);
+
+                
+                node
+            }
+        };
+
+        let mut node = Node::new(NodeKind::Command);
+
+        node.insert_left(left_node);
+        
+        
+        node.insert_right(right_node);
+        
+
+        Ok(node)
+    }
+
+    fn parse_insert(&mut self) -> Result<Node, ()> {
+        let mut token_buffer = Vec::new();
+
+        let left_node = if let Some(token) = self.tokens.pop_front() {
+            token_buffer.push(token.clone());
+
+            match token {
+                Token::String(string) => {
+                    let mut node = Node::new(NodeKind::Arg);
+                    node.value = Some(vec![string]);
+                    node
+                }
+                _ => {
+                    for token in token_buffer {
+                        self.tokens.push_front(token)
+                    }
+                    return Err(());
+                }
+            }
+        } else {
+            return Err(());
+        };
+
+        let mut node = if let Some(token) = self.tokens.pop_front() {
+            token_buffer.push(token.clone());
+
+            if token == Token::Equal {
+                Node::new(NodeKind::Insert)
+            } else {
+                for token in token_buffer {
+                    self.tokens.push_front(token)
+                }
+                return Err(());
+            }
+        } else {
+            for token in token_buffer {
+                self.tokens.push_front(token)
+            }
+            return Err(());
+        };
+
+        let right_node = match self.parse_reference() {
+            Ok(ok) => ok,
+
+            Err(_) => {
+                if let Some(token) = self.tokens.pop_front() {
+                    token_buffer.push(token.clone());
+                    match token {
+                        Token::String(string) => {
+                            let mut node = Node::new(NodeKind::Arg);
+
+                            node.value = Some(vec![string]);
+
+                            node
+                        }
+                        _ => {
+                            for token in token_buffer {
+                                self.tokens.push_front(token)
+                            }
+                            return Err(());
+                        }
+                    }
+                } else {
+                    return Err(());
+                }
+            }
+        };
+
+        node.insert_left(left_node);
+        node.insert_right(right_node);
+
+        Ok(node)
+    }
+
+    fn parse_reference(&mut self) -> Result<Node, ()> {
+        let mut node = if let Some(token) = self.tokens.pop_front() {
+            match token {
+                Token::Reference => Node::new(NodeKind::Reference),
+                _ => {
+                    self.tokens.push_front(token);
+                    return Err(());
+                }
+            }
+        } else {
+            return Err(());
+        };
+
+        let left_node = if let Some(token) = self.tokens.pop_front() {
+            match token {
+                Token::String(string) => {
+                    let mut node = Node::new(NodeKind::Arg);
+                    node.value = Some(vec![string.to_string()]);
+                    node
+                }
+                _ => {
+                    self.tokens.push_front(token);
+                    return Err(());
+                }
+            }
+        } else {
+            return Err(());
+        };
+        node.insert_left(left_node);
+
+        Ok(node)
+    }
 }
-
-// fn split(tokens: Vec<Token>, p: Token) {}
-
-// pub enum Most {
-//     Max,
-//     Min,
-// }
-
-// #[derive(Debug, Clone)]
-// pub enum NodeKind {
-//     Semicolon(usize),
-//     Pipe(usize),
-//     Insert(usize),
-//     Reference(usize),
-//     Command(usize),
-//     Arg(usize),
-//     Args(usize),
-// }
-
-// impl NodeKind {
-//     pub fn value(&self) -> usize {
-//         return match self {
-//             NodeKind::Pipe(n) => *n,
-//             NodeKind::Command(n) => *n,
-//             NodeKind::Args(n) => *n,
-//             NodeKind::Semicolon(n) => *n,
-//             NodeKind::Insert(n) => *n,
-//             NodeKind::Reference(n) => *n,
-//             NodeKind::Arg(n) => *n,
-//         };
-//     }
-// }
-
-// impl Eq for NodeKind {}
-
-// impl Ord for NodeKind {
-//     fn cmp(&self, other: &Self) -> Ordering {
-//         self.value().cmp(&other.value())
-//     }
-// }
-
-// impl PartialOrd for NodeKind {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         Some(self.value().cmp(&other.value()))
-//     }
-// }
-
-// impl PartialEq for NodeKind {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.value() == other.value()
-//     }
-// }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum NodeKind {
@@ -134,15 +250,26 @@ impl Node {
         self.right.is_some()
     }
 
-    pub fn insert(&mut self) {}
+    // pub fn insert(&mut self, node: Node) {
 
-    pub fn insert_left(&mut self) {}
+    // }
 
-    pub fn insert_right(&mut self) {}
+    pub fn insert_left(&mut self, node: Node) {
+        match self.left.clone() {
+            Some(mut left) => left.insert_left(node),
+            None => self.left = Some(Box::new(node)),
+        }
+    }
 
-    pub fn pop(&mut self){}
+    pub fn insert_right(&mut self, node: Node) {
+        match self.right.clone() {
+            Some(mut right) => right.insert_right(node),
+            None => self.right = Some(Box::new(node)),
+        }
+    }
+    pub fn pop(&mut self) {}
 
-    pub fn find(&mut self){}
+    pub fn find(&mut self) {}
 
     fn dfs(&mut self, target: Node) -> Option<Node> {
         let mut buffer = Vec::new(); //FILO
@@ -254,5 +381,63 @@ impl Node {
 // impl PartialEq for Node {
 //     fn eq(&self, other: &Self) -> bool {
 //         self.kind.value() == other.kind.value()
+//     }
+// }
+
+// let mut list = Vec::new();
+
+// let a= tokens.binary_search(&Token::Pipe);
+// println!("{:?}",a);
+// println!("{:?}",tokens);
+
+// fn split(tokens: Vec<Token>, p: Token) {}
+
+// pub enum Most {
+//     Max,
+//     Min,
+// }
+
+// #[derive(Debug, Clone)]
+// pub enum NodeKind {
+//     Semicolon(usize),
+//     Pipe(usize),
+//     Insert(usize),
+//     Reference(usize),
+//     Command(usize),
+//     Arg(usize),
+//     Args(usize),
+// }
+
+// impl NodeKind {
+//     pub fn value(&self) -> usize {
+//         return match self {
+//             NodeKind::Pipe(n) => *n,
+//             NodeKind::Command(n) => *n,
+//             NodeKind::Args(n) => *n,
+//             NodeKind::Semicolon(n) => *n,
+//             NodeKind::Insert(n) => *n,
+//             NodeKind::Reference(n) => *n,
+//             NodeKind::Arg(n) => *n,
+//         };
+//     }
+// }
+
+// impl Eq for NodeKind {}
+
+// impl Ord for NodeKind {
+//     fn cmp(&self, other: &Self) -> Ordering {
+//         self.value().cmp(&other.value())
+//     }
+// }
+
+// impl PartialOrd for NodeKind {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         Some(self.value().cmp(&other.value()))
+//     }
+// }
+
+// impl PartialEq for NodeKind {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.value() == other.value()
 //     }
 // }
