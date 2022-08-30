e0d81bb7ce91fa06d04ce8324729fcf0c8fe7f47
flucium <14249224+flucium@users.noreply.github.com>
2022-08-31 02:49:36 +0900

    rm README.md

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,8 @@ mod prompt;
 mod shell;
 mod variable;
 
-fn main(){
+fn main() {
+    let mut parser = parser::Parser::new(parser::lexer::Lexer::new("ls".chars().collect()));
+
+    println!("{:?}",parser.parse());
 }
