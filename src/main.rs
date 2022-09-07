e7a7f5e408cd7dd50769bcfeded90a86e240648f
flucium <14249224+flucium@users.noreply.github.com>
2022-09-07 12:28:51 +0900

    clean ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use shell::Shell;
 mod ansi;
 mod builtin;
-mod manifest;
 mod parser;
 mod prompt;
 mod shell;
 mod variable;
 
-use std::borrow::Borrow;
-use std::convert::TryInto;
-use std::io::stdout;
-use std::io::Read;
-use std::io::Write;
-
 fn main() {
     Shell::new().initialize().repl();
-
 }
