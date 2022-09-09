bdfcbefd4c71fe4793073f849026a4146e3eff2b
flucium <14249224+flucium@users.noreply.github.com>
2022-09-09 12:59:48 +0900

    clean ./src/builtin/mod.rs
    all comment-out the ./arc/built/mod.rs in for the time being

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -2,11 +2,11 @@
 // use std::io;
 // use std::path::Path;
 // use std::process;
-
+//
 // pub fn exit(code: i32) -> ! {
 //     process::exit(code)
 // }
-
+//
 // pub fn cd(string: &str) -> io::Result<()> {
 //     let path = Path::new(string);
 
