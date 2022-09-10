0d7f7627c37053a45ed2e619a783ebf4cc33f5a9
flucium <14249224+flucium@users.noreply.github.com>
2022-09-10 15:27:31 +0900

    update ./src/main.rs
    print manifest

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use shell::Shell;
 mod ansi;
 mod builtin;
+mod manifest;
 mod parser;
 mod prompt;
 mod shell;
-mod variable;
-mod manifest;
 mod terminal;
+mod variable;
+use std::io::{stdout, Write};
+
 fn main() {
+    stdout()
+        .lock()
+        .write_all(format!("{}{}\n", manifest::name(), manifest::version()).as_bytes())
+        .unwrap();
     Shell::new().initialize().repl();
-    
 }
