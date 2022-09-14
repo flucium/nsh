be277c372da4c92f3d98327ef997844b2438146a
flucium <14249224+flucium@users.noreply.github.com>
2022-09-14 13:59:00 +0900

    update ./src/builtin/mod.rs

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -1,11 +1,12 @@
+use crate::variable::Variable;
 use std::env;
 use std::io;
 use std::path::Path;
-// use std::process;
+use std::process;
 
-// pub fn exit(code: i32) -> ! {
-//     process::exit(code)
-// }
+pub fn exit(code: i32) -> ! {
+    process::exit(code)
+}
 
 pub fn cd(string: &str) -> io::Result<()> {
     let path = Path::new(string);
@@ -15,4 +16,18 @@ pub fn cd(string: &str) -> io::Result<()> {
     env::set_var("PWD", path);
 
     Ok(())
+}
+
+pub fn senv(v: &mut Variable) ->io::Result<String>{
+    let mut buffer = String::new();
+
+    for key in v.keys() {
+        let key = &key;
+
+        buffer.push_str(&format!("{} {}", key, v.get(key).unwrap_or_default()));
+
+        buffer.push('\n');
+    }
+
+    Ok(buffer)
 }
