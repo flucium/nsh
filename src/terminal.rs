031c72e337357c1b919548440d5710f2210372f8
flucium <14249224+flucium@users.noreply.github.com>
2022-10-07 14:07:32 +0900

    fix ./src/terminal.rs

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,15 +1,14 @@
 use crate::ansi;
 use libc;
-use std::cell::RefCell;
 use std::io;
-use std::io::{stdout, Stdout, Write};
+use std::io::{stdout, Write};
 use std::process::exit;
 
 pub struct Terminal {
     buffer: Vec<u8>,
     buffer_index: usize,
     prompt: String,
-    stdout: RefCell<Stdout>,
+    origin_termios: libc::termios,
 }
 
 impl Terminal {
@@ -19,18 +18,121 @@ impl Terminal {
             buffer_index: 0,
 
             prompt: String::new(),
-
-            stdout: RefCell::new(stdout()),
+            origin_termios: termios(),
         }
     }
 
     pub fn prompt(&mut self, prompt: String) -> &mut Terminal {
-        self.prompt = prompt.to_string();
+        self.prompt = prompt;
         self
     }
 
+    pub fn read_line(&mut self) -> io::Result<String> {
+        self.set_raw_mode();
+
+        self.init_buffer()?;
+
+        let stdout = stdout();
+
+        let mut stdout = stdout.lock();
+
+        loop {
+            stdout.flush()?;
+
+            if let Some(char) = getch() {
+                match char {
+                    0 => continue,
+                    3 => {
+                        self.unset_raw_mode();
+                        exit(0)
+                    }
+
+                    10 => break,
+
+
+                    27 => {
+                        if getch().unwrap_or(27) != 91 {
+                            continue;
+                        }
+
+                        match getch().unwrap_or(91) {
+                            //up
+                            65 => {}
+
+                            //down
+                            66 => {}
+
+                            //right
+                            67 => {
+                                if self.buffer_index < self.buffer.len() {
+                                    self.buffer_index += 1;
+                                    stdout.write_all(
+                                        format!("{}", ansi::Cursor::Right.get_esc_code())
+                                            .as_bytes(),
+                                    )?;
+                                }
+                            }
+
+                            //left
+                            68 => {
+                                if self.buffer_index > 0 {
+                                    stdout.write_all(
+                                        format!("{}", ansi::Cursor::Left.get_esc_code()).as_bytes(),
+                                    )?;
+                                    self.buffer_index -= 1;
+                                }
+                            }
+                            _ => continue,
+                        }
+                    }
+
+                    127 => {
+                        self.backspace()?;
+                    }
+
+                    _ => {
+                        self.buffer.insert(self.buffer_index, char);
+
+                        self.buffer_index += 1;
+
+                        for i in 0..self.buffer.len() {
+                            if i != 0 {
+                                stdout.write_all(
+                                    format!("{}", ansi::Cursor::Backspace.get_esc_code())
+                                        .as_bytes(),
+                                )?;
+                            }
+                        }
+
+                        stdout.write_all(
+                            format!("\r{}{}", self.prompt, String::from_utf8_lossy(&self.buffer))
+                                .as_bytes(),
+                        )?;
+
+                        if self.buffer_index < self.buffer.len() {
+                            let move_position = self.prompt.len() + self.buffer_index;
+
+                            stdout.write_all(
+                                format!("{}", ansi::Cursor::Move(move_position).get_esc_code())
+                                    .as_bytes(),
+                            )?;
+                        }
+                    }
+                }
+            }
+        }
+
+        self.unset_raw_mode();
+
+        stdout.write(b"\n")?;
+
+        Ok(String::from_utf8_lossy(&self.buffer).to_string())
+    }
+
+
     fn backspace(&mut self) -> io::Result<()> {
-        let mut stdout = self.stdout.borrow_mut().lock();
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
 
         if self.buffer_index <= 0 {
             return Ok(());
@@ -72,13 +174,16 @@ impl Terminal {
     }
 
     fn init_buffer(&mut self) -> io::Result<()> {
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+
         self.buffer.clear();
 
         self.buffer_index = 0;
 
         if self.buffer_index <= self.buffer.len() {
             let move_position = self.prompt.len() + 1;
-            self.stdout.borrow_mut().lock().write_all(
+            stdout.write_all(
                 format!(
                     "\r{}{}",
                     self.prompt,
@@ -91,116 +196,31 @@ impl Terminal {
         Ok(())
     }
 
-    pub fn read_line(&mut self) -> io::Result<String> {
-        let mut termios = termios();
+    fn set_raw_mode(&mut self) {
+        unsafe { libc::tcgetattr(0, &mut self.origin_termios) };
 
-        set_raw_mode(&mut termios);
+        let mut raw = self.origin_termios;
 
-        let mut stdout = self.stdout.borrow_mut().lock();
+        raw.c_lflag = raw.c_lflag & !(libc::ICANON | libc::ECHO | libc::IEXTEN | libc::ISIG);
 
-        self.init_buffer()?;
-        
-        
-        
-        
-        
-        loop {
-            stdout.flush().unwrap_or_default();
-            if let Some(code) = getch() {
-                match code {
-                    [0] => continue,
-                    [3] => {
-                        unset_raw_mode(&mut termios);
+        raw.c_cc[libc::VTIME] = 0;
 
-                        exit(0)
-                    }
+        raw.c_cc[libc::VMIN] = 1;
 
-                    [10] => break,
-
-                    [27] => {
-                        if getch().unwrap_or([27]) != [91] {
-                            continue;
-                        }
-
-                        match getch().unwrap_or([91]) {
-                            //up
-                            [65] => {}
-
-                            //down
-                            [66] => {}
-
-                            //right
-                            [67] => {
-                                if self.buffer_index < self.buffer.len() {
-                                    self.buffer_index += 1;
-                                    stdout.write_all(
-                                        format!("{}", ansi::Cursor::Right.get_esc_code())
-                                            .as_bytes(),
-                                    )?;
-                                }
-                            }
-                            //left
-                            [68] => {
-                                if self.buffer_index > 0 {
-                                    stdout.write_all(
-                                        format!("{}", ansi::Cursor::Left.get_esc_code()).as_bytes(),
-                                    )?;
-                                    self.buffer_index -= 1;
-                                }
-                            }
-                            _ => continue,
-                        }
-                    }
-                    [127] => {
-                        self.backspace()?;
-                    }
-                    _ => {
-                        let code = match code.get(0) {
-                            Some(code) => *code,
-                            None => continue,
-                        };
-
-                        self.buffer.insert(self.buffer_index, code);
-                        self.buffer_index += 1;
-                        for i in 0..self.buffer.len() {
-                            if i != 0 {
-                                stdout.write_all(
-                                    format!("{}", ansi::Cursor::Backspace.get_esc_code())
-                                        .as_bytes(),
-                                )?;
-                            }
-                        }
-
-                        stdout.write_all(
-                            format!("\r{}{}", self.prompt, String::from_utf8_lossy(&self.buffer))
-                                .as_bytes(),
-                        )?;
-
-                        if self.buffer_index < self.buffer.len() {
-                            let move_position = self.prompt.len() + self.buffer_index;
-
-                            stdout.write_all(
-                                format!("{}", ansi::Cursor::Move(move_position).get_esc_code())
-                                    .as_bytes(),
-                            )?;
-                        }
-                    }
-                }
-            }
-
-            
+        unsafe {
+            libc::tcsetattr(0, 0, &raw);
+            libc::fcntl(0, libc::F_SETFL);
         }
+    }
 
-        unset_raw_mode(&mut termios);
-
-        stdout.write(b"\n")?;
-
-
-        Ok(String::from_utf8_lossy(&self.buffer).to_string())
+    fn unset_raw_mode(&mut self) {
+        unsafe {
+            libc::tcsetattr(0, 0, &self.origin_termios);
+        }
     }
 }
 
-fn getch() -> Option<[u8; 1]> {
+fn getch() -> Option<u8> {
     let code = [0; 1];
 
     let n = unsafe { libc::read(0, code.as_ptr() as *mut libc::c_void, 1) };
@@ -209,31 +229,7 @@ fn getch() -> Option<[u8; 1]> {
         return None;
     }
 
-    Some(code)
-}
-
-fn unset_raw_mode(termios: &mut libc::termios) {
-    termios.c_lflag = libc::ECHO | libc::ICANON;
-
-    unsafe {
-        libc::tcsetattr(0, 0, termios);
-    }
-}
-
-fn set_raw_mode(termios: &mut libc::termios) {
-    unsafe {
-        libc::tcgetattr(0, termios);
-    };
-
-    termios.c_lflag = termios.c_lflag & !(libc::ECHO | libc::ICANON);
-    termios.c_cc[libc::VTIME] = 0;
-    termios.c_cc[libc::VMIN] = 1;
-
-    unsafe {
-        libc::tcsetattr(0, libc::TCSANOW, termios);
-        // libc::fcntl(0, libc::F_SETFL, libc::O_NONBLOCK);
-        libc::fcntl(0, libc::F_SETFL);
-    };
+    Some(code[0])
 }
 
 #[cfg(target_os = "macos")]
