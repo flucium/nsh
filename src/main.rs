ac71352181c10f087b60acf079ea743fa5fbcf91
flucium <14249224+flucium@users.noreply.github.com>
2022-10-13 10:40:29 +0900

    clean ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use shell::Shell;
 mod ansi;
 mod builtin;
 mod evaluator;
+mod history;
 mod manifest;
 mod parser;
 mod prompt;
 mod shell;
 mod terminal;
 mod variable;
-mod history;
+
 fn main() {
     Shell::new().initialize().repl();
 }
