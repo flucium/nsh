373d59053dfa6e70fdf9e0b57be5920db8384898
flucium <14249224+flucium@users.noreply.github.com>
2022-10-15 13:38:27 +0900

    update ./src/evaluator.rs

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -88,128 +88,103 @@ impl Evaluator {
                 self.variable.insert(key, val);
             }
 
-            parser::Node::Command(mut command) => {
-                let mut program = String::new();
-                let mut args = VecDeque::new();
-                // let mut ifiles = VecDeque::new();
-                // let mut ofiles = VecDeque::new();
-                // let mut efiles = VecDeque::new();
+            parser::Node::Command(command) => {
+                self.run_command(command)?;
+            }
 
-                match command.take_prefix() {
-                    Some(prefix) => match *prefix {
-                        parser::Node::String(string) => {
-                            program = string;
-                        }
+            _ => {}
+        }
 
-                        parser::Node::VReference(key) => {
-                            program = self.variable.get(key).unwrap_or_default();
-                        }
-                        _ => {}
-                    },
-                    None => {}
+        Ok(())
+    }
+
+    fn run_command(&mut self, mut command: parser::Command) -> io::Result<()> {
+        let is_child = self.child.is_some();
+
+        let (mut program, mut args): (String, VecDeque<String>) = (String::new(), VecDeque::new());
+
+        //take program
+        match command.take_prefix() {
+            Some(prefix) => match *prefix {
+                parser::Node::String(string) => program.push_str(&string),
+
+                parser::Node::VReference(key) => {
+                    program.push_str(&self.variable.get(key).unwrap_or_default())
                 }
 
-                if let Some(mut suffix) = command.take_suffix() {
-                    while let Some(node) = suffix.pop() {
-                        match node {
-                            parser::Node::String(string) => {
-                                args.push_front(string);
-                            }
-                            parser::Node::VReference(key) => {
-                                args.push_front(self.variable.get(key).unwrap_or_default());
-                            }
-                            // parser::Node::Redirect(mut redirect) => {
-                            //     let left = match redirect.take_left() {
-                            //         Some(left) => match *left {
-                            //             parser::Node::FD(fd) => fd as i32,
-                            //             _ => continue,
-                            //         },
-                            //         None => continue,
-                            //     };
+                _ => return Ok(()),
+            },
+            None => return Ok(()),
+        };
 
-                            //     // if left != 0 && matches!(redirect.kind(), RedirectKind::Input) {
-                            //     //     panic!("")
-                            //     // }
-
-                            //     let file = match redirect.take_right() {
-                            //         Some(right) => match *right {
-                            //             parser::Node::String(string) => File::options()
-                            //                 .create(true)
-                            //                 .write(true)
-                            //                 .read(true)
-                            //                 .open(string)?,
-
-                            //             parser::Node::FD(fd) => unsafe {
-                            //                 File::from_raw_fd(fd as i32)
-                            //             },
-
-                            //             _ => continue,
-                            //         },
-                            //         None => continue,
-                            //     };
-
-                            //     if matches!(left, 0 | 1 | 2) == false {
-                            //         unsafe {
-                            //             libc::dup2(left, file.as_raw_fd());
-                            //         }
-                            //     }
-
-                            //     match redirect.get_kind() {
-                            //         parser::RedirectKind::Input => {
-                            //             ifiles.push_front(file);
-                            //         }
-                            //         parser::RedirectKind::Output => {
-                            //             if left == 2 {
-                            //                 efiles.push_front(file);
-                            //             } else {
-                            //                 ofiles.push_front(file);
-                            //             }
-                            //         }
-                            //     }
-                            // }
-                            _ => {}
-                        }
+        //take suffix
+        if let Some(mut suffix) = command.take_suffix() {
+            while let Some(node) = suffix.pop() {
+                match node {
+                    parser::Node::String(string) => {
+                        args.push_front(string);
                     }
+                    parser::Node::VReference(key) => {
+                        args.push_front(self.variable.get(key).unwrap_or_default())
+                    }
+
+                    _ => {}
                 }
+            }
+        }
 
-                //^^^^
-                match program.to_lowercase().as_str() {
-                    "set" => {
-                        builtin::set(
-                            &mut self.variable,
-                            args.pop_front().unwrap_or_default(),
-                            args.pop_front().unwrap_or_default(),
-                        );
-                    }
-                    "unset" => {
-                        builtin::unset(&mut self.variable, args.pop_front().unwrap_or_default());
-                    }
+        match program.to_lowercase().as_str() {
+            "set" => {
+                let key = match args.pop_front() {
+                    Some(string) => string,
+                    None => return Ok(()),
+                };
 
-                    "cd" => {
-                        builtin::cd(args.pop_front().unwrap_or_default())?
-                        //
-                    }
+                let val = args.pop_front().unwrap_or_default();
 
-                    _ => {
-                        let is_child = self.child.is_some();
+                builtin::set(&mut self.variable, key, val);
+            }
 
-                        let mut ps_result = process::Command::new(program)
-                            .args(args)
-                            // .stdin(if is_child || ifiles.is_empty() == false {
-                            .stdin(if is_child {
-                                process::Stdio::piped()
-                            } else {
-                                process::Stdio::inherit()
-                            })
-                            // .stdout(if self.is_pipe && ofiles.is_empty() {
-                            .stdout(if self.is_pipe {
-                                process::Stdio::inherit()
-                            } else {
-                                process::Stdio::piped()
-                            })
-                            .stderr(process::Stdio::inherit())
-                            .spawn()?;
+            "unset" => {
+                let key = match args.pop_front() {
+                    Some(string) => string,
+                    None => return Ok(()),
+                };
 
+                builtin::unset(&mut self.variable, key);
+            }
+
+            "cd" => {
+                builtin::cd(args.pop_front().unwrap_or_default())?;
+            }
+
+            _ => {
+                //stdin
+                let stdin = if is_child {
+                    process::Stdio::piped()
+                } else {
+                    process::Stdio::inherit()
+                };
+
+                //stdout
+                let stdout = if self.is_pipe {
+                    process::Stdio::inherit()
+                } else {
+                    process::Stdio::piped()
+                };
+
+                //stderr
+                // if ...{...}else{...};
+                let stderr = process::Stdio::inherit();
+
+                match process::Command::new(program.as_str())
+                    .args(args)
+                    .stdin(stdin)
+                    .stdout(stdout)
+                    .stderr(stderr)
+                    .spawn()
+                {
+                    Ok(mut ps_result) => {
                         if let Some(stdin) = ps_result.stdin.as_mut() {
                             if let Some(mut child) = self.child.take() {
                                 if let Some(stdout) = child.stdout.take() {
@@ -220,245 +195,28 @@ impl Evaluator {
                                     }
                                 }
                             }
-
-                            //
                         }
 
                         if is_child {
                             ps_result.wait()?;
                         }
 
-                        self.child = Some(ps_result)
+                        self.child = Some(ps_result);
                     }
+                    Err(err) => match err.kind() {
+                        io::ErrorKind::NotFound => {
+                            io::stderr().lock().write(
+                                format!("nsh: command not found: {}\n", program).as_bytes(),
+                            )?;
+                        }
+                        _ => {
+                            Err(err)?;
+                        }
+                    },
                 }
             }
-
-            _ => {}
         }
 
         Ok(())
     }
 }
-
-// pub struct Evaluator {
-//     node: parser::Node,
-//     child: Option<process::Child>,
-//     is_pipe: bool,
-//     variable: Variable,
-// }
-
-// impl Evaluator {
-//     pub fn new(node: parser::Node) -> Self {
-//         Self {
-//             node: node,
-//             child: None,
-//             is_pipe: false,
-//             variable: Variable::new(),
-//         }
-//     }
-
-//     pub fn variable(&mut self, variable: Variable) {
-//         self.variable = variable;
-//     }
-
-//     pub fn get_variable(&mut self) -> &Variable {
-//         &self.variable
-//     }
-
-//     pub fn wait(&mut self) -> io::Result<()> {
-//         if let Some(mut child) = self.child.take() {
-//             child.wait()?;
-//         }
-//         Ok(())
-//     }
-
-//     pub fn eval(&mut self) -> io::Result<()> {
-//         match self.node.to_owned() {
-//             parser::Node::Pipe(pipe) => {
-//                 let mut pipe = pipe.peekable();
-
-//                 while let Some(node) = pipe.next() {
-//                     self.is_pipe = !pipe.peek().is_some();
-
-//                     self.node = node;
-
-//                     self.eval()?;
-//                 }
-//             }
-
-//             parser::Node::VInsert(mut vinsert) => {
-//                 let key = if let Some(key) = vinsert.take_key() {
-//                     match *key {
-//                         parser::Node::String(string) => string,
-//                         _ => return Ok(()),
-//                     }
-//                 } else {
-//                     return Ok(());
-//                 };
-
-//                 let val = if let Some(key) = vinsert.take_val() {
-//                     match *key {
-//                         parser::Node::String(string) => string,
-//                         _ => return Ok(()),
-//                     }
-//                 } else {
-//                     return Ok(());
-//                 };
-
-//                 self.variable.insert(key, val);
-//             }
-
-//             parser::Node::Command(mut command) => {
-//                 let mut program = String::new();
-//                 let mut args = VecDeque::new();
-//                 let mut ifiles = VecDeque::new();
-//                 let mut ofiles = VecDeque::new();
-//                 let mut efiles = VecDeque::new();
-
-//                 match command.take_prefix() {
-//                     Some(prefix) => match *prefix {
-//                         parser::Node::String(string) => {
-//                             program = string;
-//                         }
-
-//                         parser::Node::VReference(key) => {
-//                             program = self.variable.get(key).unwrap_or_default();
-//                         }
-//                         _ => {}
-//                     },
-//                     None => {}
-//                 }
-
-//                 if let Some(mut suffix) = command.take_suffix() {
-//                     while let Some(node) = suffix.pop() {
-//                         match node {
-//                             parser::Node::String(string) => {
-//                                 args.push_front(string);
-//                             }
-//                             parser::Node::VReference(key) => {
-//                                 args.push_front(self.variable.get(key).unwrap_or_default());
-//                             }
-//                             parser::Node::Redirect(mut redirect) => {
-//                                 let left = match redirect.take_left() {
-//                                     Some(left) => match *left {
-//                                         parser::Node::FD(fd) => fd as i32,
-//                                         _ => continue,
-//                                     },
-//                                     None => continue,
-//                                 };
-
-//                                 // if left != 0 && matches!(redirect.kind(), RedirectKind::Input) {
-//                                 //     panic!("")
-//                                 // }
-
-//                                 let file = match redirect.take_right() {
-//                                     Some(right) => match *right {
-//                                         parser::Node::String(string) => File::options()
-//                                             .create(true)
-//                                             .write(true)
-//                                             .read(true)
-//                                             .open(string)?,
-
-//                                         parser::Node::FD(fd) => unsafe {
-//                                             File::from_raw_fd(fd as i32)
-//                                         },
-
-//                                         _ => continue,
-//                                     },
-//                                     None => continue,
-//                                 };
-
-//                                 if matches!(left, 0 | 1 | 2) == false {
-//                                     unsafe {
-//                                         libc::dup2(left, file.as_raw_fd());
-//                                     }
-//                                 }
-
-//                                 match redirect.get_kind() {
-//                                     parser::RedirectKind::Input => {
-//                                         ifiles.push_front(file);
-//                                     }
-//                                     parser::RedirectKind::Output => {
-//                                         if left == 2 {
-//                                             efiles.push_front(file);
-//                                         } else {
-//                                             ofiles.push_front(file);
-//                                         }
-//                                     }
-//                                 }
-//                             }
-//                             _ => {}
-//                         }
-//                     }
-//                 }
-
-//                 //^^^^
-//                 match program.to_lowercase().as_str() {
-//                     "set" => {
-//                         builtin::set(
-//                             &mut self.variable,
-//                             args.pop_front().unwrap_or_default(),
-//                             args.pop_front().unwrap_or_default(),
-//                         );
-//                     }
-//                     "unset" => {
-//                         builtin::unset(&mut self.variable, args.pop_front().unwrap_or_default());
-//                     }
-
-//                     "cd" => {
-//                         builtin::cd(args.pop_front().unwrap_or_default())?
-//                         //
-//                     }
-
-//                     _ => {
-//                         let is_child = self.child.is_some();
-
-//                         let mut ps_result = process::Command::new(program)
-//                             .args(args)
-//                             .stdin(if is_child || ifiles.is_empty() == false {
-//                                 process::Stdio::piped()
-//                             } else {
-//                                 process::Stdio::inherit()
-//                             })
-//                             .stdout(if self.is_pipe && ofiles.is_empty() {
-//                                 process::Stdio::inherit()
-//                             } else {
-//                                 process::Stdio::piped()
-//                             })
-//                             .stderr(process::Stdio::inherit())
-//                             .spawn()?;
-
-//                         if let Some(stdin) = ps_result.stdin.as_mut() {
-//                             if let Some(mut child) = self.child.take() {
-//                                 if let Some(stdout) = child.stdout.take() {
-//                                     let bytes = stdout.bytes();
-//                                     for byte in bytes {
-//                                         let byte = vec![byte.unwrap()];
-//                                         stdin.write(&byte)?;
-//                                     }
-//                                 }
-//                             }
-
-//                             //
-//                             for mut ifile in ifiles {
-//                                 let mut buffer = Vec::new();
-//                                 ifile.read_to_end(&mut buffer)?;
-//                                 stdin.write_all(&buffer)?;
-//                             }
-//                         }
-
-//                         if is_child {
-//                             ps_result.wait()?;
-//                         }
-
-//                         self.child = Some(ps_result)
-//                     }
-//                 }
-//             }
-
-//             _ => {}
-//         }
-
-//         Ok(())
-//     }
-// }
