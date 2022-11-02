74fbfa832ed03c68ddd14373f31e32c3e7d245ba
flucium <14249224+flucium@users.noreply.github.com>
2022-11-02 10:59:16 +0900

    add ./src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// use nsh::*;
+
+fn main() {
+    // let tree = parser::Parser::new(parser::lexer::Lexer::new(
+    //     "ping -c 10 flucium.commmmm > test.txt 2> err.txt;cat < err.txt;".chars().collect(),
+    // ))
+    // .parse()
+    // .unwrap();
+
+    // evaluator::Evaluator::new(parser::Node::Tree(tree))
+    //     .eval()
+    //     .unwrap();
+}
