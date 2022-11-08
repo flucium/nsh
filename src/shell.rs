ea0fb02fc7dac674761421ad4cf7b50f415c3fa1
flucium <14249224+flucium@users.noreply.github.com>
2022-11-08 13:30:42 +0900

    update ./src/shell.rs

--- src/shell.rs
+++ src/shell.rs
@@ -7,12 +7,11 @@ use crate::parser::Parser;
 use crate::prompt;
 use crate::terminal::Terminal;
 use crate::variable::Variable;
+use crate::profile;
 use std::env;
-use std::fs::File;
 use std::io;
-use std::io::Read;
 use std::io::Write;
-use std::path::PathBuf;
+
 
 pub struct Shell {
     terminal: Terminal,
@@ -27,40 +26,21 @@ impl Shell {
         }
     }
 
-    pub fn initialize(&mut self) -> &mut Self {
-        let local_profile = Profile::new(ProfileKind::Local);
+    pub fn initialize(&mut self) -> Result<&mut Self >{
 
-        let source = match if local_profile.exists() {
-            local_profile.read()
-        } else {
-            local_profile.create()
-        } {
-            Ok(profile) => profile,
-            Err(err) => {
-                panic!("{err}");
-            }
-        };
-
-        let node = match parse(source) {
-            Ok(node) => node,
-            Err(err) => panic!("{err}"),
-        };
+        let node = parse( profile::read()?) ?;
 
         let mut evaluator = Evaluator::new(node);
 
         evaluator.set_variable(self.variable.to_owned());
 
-        if let Err(err) = evaluator.eval() {
-            io::stderr()
-                .write_all(format!("{err}\n").as_bytes())
-                .unwrap();
-        }
+        evaluator.eval()?;
 
         self.variable = evaluator.variable().to_owned();
 
         self.init_history();
 
-        self
+        Ok(self)
     }
 
     fn init_history(&mut self) {
@@ -122,55 +102,4 @@ fn parse(source: String) -> Result<parser::Node> {
     .parse()
 }
 
-struct Profile(ProfileKind);
 
-impl Profile {
-    fn new(profile_type: ProfileKind) -> Self {
-        Self { 0: profile_type }
-    }
-
-    fn exists(&self) -> bool {
-        if let Ok(path) = self.lookup() {
-            return path.exists();
-        }
-
-        false
-    }
-
-    fn create(&self) -> io::Result<String> {
-        const DEFAULT_VALUE: &str = "NSH_PROMPT = \"w\\# \"";
-
-        let path = self.lookup()?;
-
-        File::create(path)?.write_all(DEFAULT_VALUE.as_bytes())?;
-
-        Ok(DEFAULT_VALUE.to_string())
-    }
-
-    fn read(&self) -> io::Result<String> {
-        let path = self.lookup()?;
-
-        let mut buffer = Vec::new();
-
-        File::open(path)?.read_to_end(&mut buffer)?;
-
-        Ok(String::from_utf8_lossy(&buffer).to_string())
-    }
-
-    fn lookup(&self) -> io::Result<PathBuf> {
-        match self.0 {
-            ProfileKind::Local => match env::var("HOME") {
-                Ok(val) => {
-                    let mut path = PathBuf::from(val);
-                    path.push(".nsh_profile");
-                    Ok(path)
-                }
-                Err(err) => panic!("{err}"),
-            },
-        }
-    }
-}
-
-enum ProfileKind {
-    Local,
-}
