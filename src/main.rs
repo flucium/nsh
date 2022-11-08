20d192d72d777e23d329a745a133aeccda4b5d0c
flucium <14249224+flucium@users.noreply.github.com>
2022-11-08 13:29:46 +0900

    update ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io::stderr;
+use std::io::Write;
 
-fn main(){
-    nsh::shell::Shell::new().initialize().repl();
+fn main() {
+    match nsh::shell::Shell::new().initialize() {
+        Ok(ok) => ok.repl(),
+        Err(err) => stderr()
+            .lock()
+            .write_all(format!("{err}").as_bytes())
+            .unwrap(),
+    }
 }
