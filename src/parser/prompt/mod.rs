a6a41a6d10313f7bb2dceb7d1b882c776587d917
flucium <14249224+flucium@users.noreply.github.com>
2022-11-11 13:03:12 +0900

    update ./src/parser/prompt/mod.rs

--- src/parser/prompt/mod.rs
+++ src/parser/prompt/mod.rs
@@ -1,4 +1,23 @@
+use std::env;
 
-pub fn parse(source:String){
-    
+pub fn parse(source: String) -> String {
+    source
+        .replace("\\W", &get_current_dir_path(true))
+        .replace("\\w", &get_current_dir_path(false))
+}
+
+fn get_current_dir_path(is_full_path: bool) -> String {
+    match env::current_dir() {
+        Ok(path) => {
+            if is_full_path == false {
+                path.file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string()
+            } else {
+                path.to_string_lossy().to_string()
+            }
+        }
+        Err(_) => "/".to_owned(),
+    }
 }
