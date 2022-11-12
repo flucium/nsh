0c5ea74a43a3351472a4fbb7a424c38f9a1206fb
flucium <14249224+flucium@users.noreply.github.com>
2022-11-12 12:35:38 +0900

    update ./src/variable.rs
    added fn append(...){...}

--- src/variable.rs
+++ src/variable.rs
@@ -2,24 +2,40 @@ use std::collections::HashMap;
 
 pub struct Variable(HashMap<String, String>);
 
+impl ToOwned for Variable {
+    type Owned = Variable;
+
+    fn to_owned(&self) -> Self::Owned {
+        Self {
+            0: self.0.to_owned(),
+        }
+    }
+}
+
 impl Variable {
     pub fn new() -> Self {
         Self { 0: HashMap::new() }
     }
 
-    pub fn variable(&mut self) -> Self {
-        Self {
-            0: self.0.to_owned(),
+    pub fn append(&mut self, key: String, val: String) {
+        match self.0.get_mut(&key) {
+            Some(string) => {
+                string.push(':');
+                string.push_str(&val)
+            }
+            None => {
+                self.0.insert(key, val);
+            }
         }
     }
 
-    pub fn insert(&mut self,key:String,val:String){
+    pub fn insert(&mut self, key: String, val: String) {
         self.0.insert(key, val);
     }
 
     //pub fn get(&self,key:String)->Option<&String>{
     //    self.0.get(&key)
-    pub fn get(&self,key:String)->Option<&str>{
+    pub fn get(&self, key: String) -> Option<&str> {
         self.0.get(&key).map(|x| &**x)
     }
 }
