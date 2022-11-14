cc8a39b07e79aa11b59661405163b7c67fbd0b9e
flucium <14249224+flucium@users.noreply.github.com>
2022-11-15 01:14:50 +0900

    update ./src/profile.rs
    added xxx_from

--- src/profile.rs
+++ src/profile.rs
@@ -2,6 +2,7 @@ use crate::error::*;
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 use std::path::PathBuf;
 fn get_path() -> Result<PathBuf> {
     match env::var("HOME").or(env::var("USER")) {
@@ -32,5 +33,34 @@ pub fn read() -> Result<String> {
             Ok(buffer)
         }
         Err(err) => Err(Error::new(ErrorKind::OpenFailed, err.to_string())),
+    }
+}
+
+pub fn create_from(path: &Path) -> Result<()> {
+    if let Err(err) = File::create(path) {
+        return Err(Error::new(ErrorKind::CreateFailed, err.to_string()));
+    }
+    Ok(())
+}
+
+pub fn read_from(path: &Path) -> Result<String> {
+    match File::open(path) {
+        Ok(mut file) => {
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer).unwrap();
+            Ok(buffer)
+        }
+        Err(err) => Err(Error::new(ErrorKind::OpenFailed, err.to_string())),
+    }
+}
+
+pub fn exists_from(path:&Path)->bool{
+    path.exists()
+}
+
+pub fn exists() -> bool {
+    match get_path() {
+        Ok(path) => path.exists(),
+        Err(_) => false,
     }
 }
