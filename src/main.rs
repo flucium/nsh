0d23d8868841e66a479161b96ec33923ebc442ef
flucium <14249224+flucium@users.noreply.github.com>
2022-11-15 01:15:21 +0900

    clean ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+// use clap::Arg;
+// use clap::ArgAction;
+// use clap::Command;
 use std::io::stderr;
 use std::io::Write;
+
+// fn clap_app() -> Command {
+//     Command::new(nsh::manifest::name())
+//         .version(nsh::manifest::version())
+//         .author(nsh::manifest::author())
+//         .arg(
+//             Arg::new("profile")
+//                 .long("profile")
+//                 .short('p')
+//                 .action(ArgAction::Set)
+//                 .required(false),
+//         )
+// }
 
 fn main() {
     match nsh::shell::Shell::new().initialize() {
